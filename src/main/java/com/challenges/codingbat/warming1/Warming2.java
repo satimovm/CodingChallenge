27059a20b6c068669ffbabcdc3cf8d00d0544561
Murad Satimov <murad.satimov@gmail.com>
2018-09-28 17:28:39 +0500

    warming 2

---
+++ src/main/java/com/challenges/codingbat/warming1/Warming2.java
@@ -0,0 +1,27 @@
+package com.challenges.codingbat.warming1;
+
+/**
+ * Created by murad
+ * Date: 9/28/18.
+ */
+public class Warming2 {
+    /**
+     * Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+     * <p>
+     * diff21(19) → 2
+     * diff21(10) → 11
+     * diff21(21) → 0
+     * </p>
+     */
+    public static void main(String[] args) {
+        System.out.println("Result: " + diff21(19));
+        System.out.println("Result: " + diff21(10));
+        System.out.println("Result: " + diff21(21));
+    }
+
+    private static int diff21(int n) {
+        return n > 21 ? (Math.abs(21 - n)) * 2 : Math.abs(21 - n);
+    }
+
+
+}
