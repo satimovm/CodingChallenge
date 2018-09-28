4e958db84069a8f259fdffb985eba4ed459b1f30
Murad Satimov <murad.satimov@gmail.com>
2018-09-28 17:26:44 +0500

    warming 1

--- src/main/java/com/challenges/codingbat/warming1/Warming1.java
+++ src/main/java/com/challenges/codingbat/warming1/Warming1.java
@@ -5,7 +5,23 @@ package com.challenges.codingbat.warming1;
  * Date: 9/28/18.
  */
 public class Warming1 {
+    /**
+     * Problem:
+     * The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+     * We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+     * <p>
+     * sleepIn(false, false) → true
+     * sleepIn(true, false) → false
+     * sleepIn(false, true) → true
+     */
     public static void main(String[] args) {
-
+        System.out.println("Result: " + sleepIn(false, false)); //→ true
+        System.out.println("Result: " + sleepIn(true, false)); //→ false
+        System.out.println("Result: " + sleepIn(false, true)); //→ true
     }
+
+    private static boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
 }
